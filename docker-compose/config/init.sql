0e1ca8419bbf6318745570ca9e53e581e19e3d82
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2022-05-27 17:14:08 +0200

    cse export process deployment resources adaptation (still in progress)

--- docker-compose/config/init.sql
+++ docker-compose/config/init.sql
@@ -7,6 +7,13 @@ GRANT ALL PRIVILEGES ON DATABASE cse_idcc_tasks TO cse_idcc_server;
 CREATE DATABASE cse_d2cc_tasks;
 CREATE USER cse_d2cc_server WITH ENCRYPTED PASSWORD 'cse-d2cc';
 GRANT ALL PRIVILEGES ON DATABASE cse_d2cc_tasks TO cse_d2cc_server;
+CREATE DATABASE cse_export_idcc_tasks;
+CREATE USER cse_export_idcc_server WITH ENCRYPTED PASSWORD 'cse-export-idcc';
+GRANT ALL PRIVILEGES ON DATABASE cse_export_idcc_tasks TO cse_export_idcc_server;
+CREATE DATABASE cse_export_d2cc_tasks;
+CREATE USER cse_export_d2cc_server WITH ENCRYPTED PASSWORD 'cse-export-d2cc';
+GRANT ALL PRIVILEGES ON DATABASE cse_export_d2cc_tasks TO cse_export_d2cc_server;
 CREATE DATABASE core_valid_tasks;
 CREATE USER core_valid_server WITH ENCRYPTED PASSWORD 'core-valid';
 GRANT ALL PRIVILEGES ON DATABASE core_valid_tasks TO core_valid_server;
+
