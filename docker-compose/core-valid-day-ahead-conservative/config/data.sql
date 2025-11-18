a17c9a138b4f969ab2ae7283f33c1353d3f17e29
schwitzguebelmar <marc.schwitzguebel_externe@rte-france.com>
2025-11-18 11:24:58 +0100

    fix typo in data sql file

--- docker-compose/core-valid-day-ahead-conservative/config/data.sql
+++ docker-compose/core-valid-day-ahead-conservative/config/data.sql
@@ -19,7 +19,7 @@ VALUES ('MIN_RAM_MCCC', 'MIN RAM MCCC PERCENT', 3, 'INT', 'BRANCH MAX IVA', 2, '
 UPDATE SET name = 'MIN RAM MCCC PERCENT', display_order = 3, parameter_type = 'INT', section_title = 'BRANCH MAX IVA', section_order = 2;
 
 INSERT INTO parameter (id, name, display_order, parameter_type, section_title, section_order, parameter_value)
-    ON CONFLICT (id) DOVALUES ('EXCLUDED_BRANCHES', 'EXCLUDED BRANCHES', 4, 'STRING', 'BRANCH MAX IVA', 2,
+    VALUES ('EXCLUDED_BRANCHES', 'EXCLUDED BRANCHES', 4, 'STRING', 'BRANCH MAX IVA', 2,
                                '[FR-FR] Creys - Saint-Vulbas 2 [DIR];[FR-FR] Creys - Saint-Vulbas 2 [OPP];[FR-CH] Cornier - Riddes [DIR];[FR-CH] Cornier - Riddes [OPP];[FR-FR] Creys - Genissiat 1 [DIR];[FR-FR] Creys - Genissiat 1 [OPP];[FR-FR] Creys - Saint-Vulbas 1 [DIR];[FR-FR] Creys - Saint-Vulbas 1 [OPP];[FR-FR] Frasnes - Genissiat [DIR];[FR-FR] Frasnes - Genissiat [OPP];[FR-FR] Creys - Genissiat 2 [DIR];[FR-FR] Creys - Genissiat 2 [OPP];[FR-CH] Cornier - Saint-Triphon [DIR];[FR-CH] Cornier - Saint-Triphon [OPP]')
-
+    ON CONFLICT (id) DO
 UPDATE SET name = 'EXCLUDED BRANCHES', display_order = 4, parameter_type = 'STRING', section_title = 'BRANCH MAX IVA', section_order = 2;
