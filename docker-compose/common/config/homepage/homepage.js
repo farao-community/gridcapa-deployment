1b3f907c094cf8a1bdbdbd70434f28829203349e
Antoine LIMOUZIN <antoine.limouzin@rte-france.com>
2026-02-10 17:52:43 +0100

    Rabbit remarks

--- docker-compose/common/config/homepage/homepage.js
+++ docker-compose/common/config/homepage/homepage.js
@@ -1,16 +1,19 @@
 fetch('apps-metadata.json')
     .then(res => res.json())
     .then(res => buildMenu(res))
+    .catch(err => console.error('Failed to load apps metadata:', err))
 
 let buildMenu = (apps) => {
     const menu = document.getElementById('gridcapa-homepage-menu');
+    const fragment = document.createDocumentFragment();
     for (const app of apps) {
-        menu.innerHTML +=
-            '<li><a href="'
-            + app.url
-            + '" style="color: '
-            + app.appColor + '">'
-            + app.name.replace('Capa ', '')
-            + '</a></li>';
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = app.url;
+        a.style.color = app.appColor;
+        a.textContent = app.name.replace('Capa ', '');
+        li.appendChild(a);
+        fragment.appendChild(li);
     }
+    menu.appendChild(fragment);
 }
