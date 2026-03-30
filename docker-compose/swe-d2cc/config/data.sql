33d91a61151f3fac6dc79c53ad487fb9cbf82a59
Antoine LIMOUZIN <antoine.limouzin@rte-france.com>
2026-03-30 17:04:12 +0200

    Take PR comments into account

--- docker-compose/swe-d2cc/config/data.sql
+++ docker-compose/swe-d2cc/config/data.sql
@@ -115,6 +115,6 @@ VALUES ('EXPORT_FIRST_UNSECURE_SHIFTED_CGM', 'Export First Unsecure shifted CGM'
 UPDATE SET name = 'Export First Unsecure shifted CGM', display_order = 10, parameter_type = 'BOOLEAN', section_title = 'Other parameters', section_order = 7;
 
 INSERT INTO parameter (id, name, display_order, parameter_type, section_title, section_order, parameter_value)
-VALUES ('RUN_GLSK_CHECKS_FIRST', 'Run GLSK checks before load flow computation', 11, 'BOOLEAN', 'Other parameters', 7, 'true')
+VALUES ('RUN_GLSK_CHECKS_BEFORE_LOADFLOW', 'Run GLSK checks before load flow computation', 11, 'BOOLEAN', 'Other parameters', 7, 'true')
     ON CONFLICT (id) DO
 UPDATE SET name = 'Run GLSK checks before load flow computation', display_order = 11, parameter_type = 'BOOLEAN', section_title = 'Other parameters', section_order = 7;
