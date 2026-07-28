876f2ff6ffae794e155b0450c049f2e4cf45f3e8
schwitzguebelmar <marc.schwitzguebel_externe@rte-france.com>
2026-07-28 17:21:29 +0200

    add parameters to docker gridcapa deployment

---
+++ docker-compose/core-valid-intraday/config/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO parameter (id, name, display_order, parameter_type, section_title, section_order, parameter_value)
+VALUES ('MAX_SELECTED_VERTICES', 'NUMBER OF MAX SELECTED VERTICES', 1, 'INT', 'VERTICES FILTERING', 1, '6')
+    ON CONFLICT (id) DO
+UPDATE SET name = 'NUMBER OF MAX SELECTED VERTICES', display_order = 1, parameter_type = 'INT', section_title = 'VERTICES FILTERING', section_order = 1;
+INSERT INTO parameter (id, name, display_order, parameter_type, section_title, section_order, parameter_value)
+VALUES ('MARGIN_FOR_PREFILTER', 'MARGIN FOR PREFILTER' , 2, 'INT', 'VERTICES FILTERING', 1, '0')
+    ON CONFLICT (id) DO
+UPDATE SET name = 'NUMBER OF MAX SELECTED VERTICES', display_order = 2, parameter_type = 'INT', section_title = 'VERTICES FILTERING', section_order = 1;
+INSERT INTO parameter (id, name, display_order, parameter_type, section_title, section_order, parameter_value)
+VALUES ('FRM_MARGIN_PERCENTAGE', 'FRM MARGIN PERCENTAGE' , 1, 'INT', 'COMPUTE IVA', 2, '5')
+    ON CONFLICT (id) DO
+UPDATE SET name = 'NUMBER OF MAX SELECTED VERTICES', display_order = 1, parameter_type = 'INT', section_title = 'COMPUTE IVA', section_order = 1;
+INSERT INTO parameter (id, name, display_order, parameter_type, section_title, section_order, parameter_value)
+VALUES ('MIN_RAM_MCCC', 'MINIMUM RAM MCCC' , 2, 'INT', 'COMPUTE IVA', 2, '20')
+    ON CONFLICT (id) DO
+UPDATE SET name = 'MINIMUM RAM MCCC', display_order = 2, parameter_type = 'INT', section_title = 'COMPUTE IVA', section_order = 1;
