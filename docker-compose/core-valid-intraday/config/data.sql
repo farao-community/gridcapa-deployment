90ae8329280a25e1b89157f831eaacd0d7fd3d1e
schwitzguebelmar <marc.schwitzguebel_externe@rte-france.com>
2026-07-29 11:22:28 +0200

    fix typo

--- docker-compose/core-valid-intraday/config/data.sql
+++ docker-compose/core-valid-intraday/config/data.sql
@@ -15,14 +15,14 @@ VALUES ('MIN_RAM_MCCC', 'MINIMUM RAM MCCC' , 2, 'INT', 'COMPUTE IVA', 2, '20')
     ON CONFLICT (id) DO
 UPDATE SET name = 'MINIMUM RAM MCCC', display_order = 2, parameter_type = 'INT', section_title = 'COMPUTE IVA', section_order = 2;
 INSERT INTO parameter (id, name, display_order, parameter_type, section_title, section_order, parameter_value)
-VALUES ('PONDERATION_CLOSEST', 'PONDERATION CLOSEST' , 1, 'INT', 'VERTICE SELECTION PONDERATION', 3, '33')
+VALUES ('PONDERATION_CLOSEST', 'PONDERATION CLOSEST' , 1, 'INT', 'VERTICES SELECTION PONDERATION', 3, '33')
     ON CONFLICT (id) DO
-UPDATE SET name = 'PONDERATION CLOSEST', display_order = 1, parameter_type = 'INT', section_title = 'VERTICE SELECTION PONDERATION', section_order = 3;
+UPDATE SET name = 'PONDERATION CLOSEST', display_order = 1, parameter_type = 'INT', section_title = 'VERTICES SELECTION PONDERATION', section_order = 3;
 INSERT INTO parameter (id, name, display_order, parameter_type, section_title, section_order, parameter_value)
-VALUES ('PONDERATION_ANGLE', 'PONDERATION ANGLE' , 2, 'INT', 'VERTICE SELECTION PONDERATION', 3, '33')
+VALUES ('PONDERATION_ANGLE', 'PONDERATION ANGLE' , 2, 'INT', 'VERTICES SELECTION PONDERATION', 3, '33')
     ON CONFLICT (id) DO
-UPDATE SET name = 'PONDERATION ANGLE', display_order = 2, parameter_type = 'INT', section_title = 'VERTICE SELECTION PONDERATION', section_order = 3;
+UPDATE SET name = 'PONDERATION ANGLE', display_order = 2, parameter_type = 'INT', section_title = 'VERTICES SELECTION PONDERATION', section_order = 3;
 INSERT INTO parameter (id, name, display_order, parameter_type, section_title, section_order, parameter_value)
-VALUES ('PONDERATION_CONSTRAINED', 'PONDERATION CONSTRAINED' , 3, 'INT', 'VERTICE SELECTION PONDERATION', 3, '34')
+VALUES ('PONDERATION_CONSTRAINED', 'PONDERATION CONSTRAINED' , 3, 'INT', 'VERTICES SELECTION PONDERATION', 3, '34')
     ON CONFLICT (id) DO
-UPDATE SET name = 'PONDERATION CONSTRAINED', display_order = 3, parameter_type = 'INT', section_title = 'VERTICE SELECTION PONDERATION', section_order = 3;
+UPDATE SET name = 'PONDERATION CONSTRAINED', display_order = 3, parameter_type = 'INT', section_title = 'VERTICES SELECTION PONDERATION', section_order = 3;
